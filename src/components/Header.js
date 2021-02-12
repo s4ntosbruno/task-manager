5838d1b72eff1607be15ea43de02c2904ab010fc
Bruno <s4ntosbruno@gmail.com>
2021-02-12 12:50:56 -0300

    react-router adicionado e json server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
-const Header = ({ title }) => {
-
-  const onClick = () => {
-    console.log('click')
-  }
+const Header = ({ onAdd, showAdd }) => {
+  const location = useLocation()
 
   return (
     <header className="header">
-      <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick}/>
+      <h1>Task Tracker</h1>
+      {location.pathname === '/' && <Button 
+        color={showAdd ? 'red' : 'green'} 
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />}
     </header>
   )
 }
